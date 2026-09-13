483840bb66b83e9bff76ff019857cbea6e71f1a9
Robera Kumsa <roberakumsa1@gmail.com>
2026-09-13 17:10:33 +0300

    update nodemailer.

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -24,8 +24,7 @@ for (const key of requiredEnv) {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465,
-
+  secure: false,
   family: 4,
 
   auth: {
